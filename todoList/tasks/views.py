ba408184447be71ae8d5628602fe1ad445071308
Miraç Doğan <miracdogan7247@gmail.com>
2024-06-12 09:29:11 +0300

    todoList
    todoList Project with Python-Django

---
+++ todoList/tasks/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import *
+# Create your views here.
+
+
+def index(request):
+
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+
+
+
+    context = {'tasks':tasks,'form':form}
+    return render(request,'index.html',context)
+
+
+def updateTask(request,pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance=task)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context =  {'form':form}
+
+    return render(request,'update_task.html',context)
+
+
+def deleteTask(request,pk):
+    item = Task.objects.get(id=pk)
+    context = {'item':item}
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect("/")
+
+    return render(request,'delete.html',context)
